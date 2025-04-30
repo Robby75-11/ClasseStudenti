0c4f8f6b2ff1370646876d975c34abbe3f70b277
Roberto Albergo <ralbergo7@gmail.com>
2025-04-30 16:59:31 +0200

    mod

---
+++ prova.java
@@ -0,0 +1,13 @@
+public class prova {
+public  String nome;
+public String dato;
+public int    number;
+// Costruttore
+    public  prova(String nome, String dato, int number) {
+        this.nome = nome;
+        this.dato = dato;
+        this.number = number;
+    }
+
+
+}
